628bdbdbea837ad913827bda0ae181b3a90baead
Marco Bozzi <marcobo97@hotmail.it>
2023-09-10 16:08:52 +0200

    Login

--- angular_volume/src/app/core/services/token.service.ts
+++ angular_volume/src/app/core/services/token.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenService {
+  private readonly TOKEN_KEY = 'auth_token';
 
-  constructor() { }
+  saveToken(token: string) {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  removeToken() {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
 }
