10e2a49296ca85934964d6e7029991c8863bdc80
Marco Bozzi <marcobo97@hotmail.it>
2023-09-11 17:08:13 +0200

    signup comp path fix

--- angular_volume/src/app/public/public-routing.module.ts
+++ angular_volume/src/app/public/public-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from './components/signup/signup/signup.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent }
