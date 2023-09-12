37c646789bbe33e59f7df6de8c958d1402be5fd9
Marco Bozzi <marcobo97@hotmail.it>
2023-09-12 16:45:00 +0200

    login + service + css

--- angular_volume/src/app/core/services/login.service.ts
+++ angular_volume/src/app/core/services/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../models/user.model';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,19 @@ export class LoginService {
 
   constructor(private http: HttpClient) { }
 
+  setUser(name: string){
+    localStorage.setItem('user', name);
+  }
+
+  getUser(): string | null {
+    return localStorage.getItem('user');
+  }
+
   login(email: string, password: string) {
-    if (email === 'prova' && password === '123')
+    if (email === 'prova' && password === '123'){
+      this.setUser(email);
       return 200;
+    }
     else
       return 403;
     //return this.http.post('http://localhost:3000/auth/login', { email, password });
