131382df002a301dc3c695f45be6a6e1ccec8e85
mrbff <marco.baffo@live.it>
2023-09-25 17:28:27 +0200

    ms

--- nest_volume/src/auth/dto/auth.dto.ts
+++ nest_volume/src/auth/dto/auth.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+
 export class AuthDto {
+    @IsNotEmpty()
+    @IsString()
     username: string;
+    
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @IsString()
     password: string;
 }
