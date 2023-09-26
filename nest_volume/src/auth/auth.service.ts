3910daceaece1e1cf85b4a10308292814dd6ff85
mrbff <marco.baffo@live.it>
2023-09-26 02:49:23 +0200

    auth wip 5603

--- nest_volume/src/auth/auth.service.ts
+++ nest_volume/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
@@ -8,42 +8,78 @@ import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+    
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+    const hash = await this.hashData(dto.password);
 
-    hashData(data: string) {
-        return bcrypt.hash(data, 10);
+    const newUser = await this.prisma.users.create({
+        data: {
+            username: dto.username,
+            email: dto.email,
+            hash,
+        },
+    });
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
+  }
+    
+  signinLocal() {}
+
+  logout() {}
+
+  refreshTokens() {}
+
+  
+
+
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'at-secret',
+          expiresIn: 60 * 15,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'rt-secret',
+          expiresIn: 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
     }
+  }
 
-    async getTokens(userId: number, email: string) {
-        const [at, rt] = await Promise.all([
+  async updateRtHash(userId: number, rt: string) {
+    const hash =  await this.hashData(rt);
+    await this.prisma.users.update({
+      where: {
+        id: userId
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
+  }
 
-        ])
-        const accessToken = this.jwtService.signAsync(
-          {
-            sub: userId,
-            email,
-          },
-          {
-            expiresIn: 60 * 15,
-          },
-        );
-    }
-
-    async signupLocal(dto: AuthDto): Promise<Tokens> {
-        const hash = await this.hashData(dto.password);
-
-        const newUser = this.prisma.users.create({
-            data: {
-                username: dto.username,
-                email: dto.email,
-                hash,
-            },
-        });
-    }
-
-    signinLocal() {}
-
-    logout() {}
-
-    refreshTokens() {}
 }
