b7d86142cd439a850d5ad7172b64a3bc68558ce3
graiolo <you@example.com>
2024-02-16 23:56:46 +0100

    add changion stutus in chat

--- angular_volume/src/app/features/components/chat/components/message/message.component.ts
+++ angular_volume/src/app/features/components/chat/components/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, NgModule } from '@angular/core';
 import { UserService } from '../../../../../core/services/user.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { InvitesService } from 'src/app/core/services/game-invite.service';
@@ -15,7 +15,7 @@ export class MessageComponent implements OnInit {
   currentUser!: boolean;
   otherUser!: boolean;
   isModerator!: boolean;
-  currentTime$: Observable<string>;
+  currentTime$: Observable<Date>;
   private subscription: Subscription;
 
   constructor(
@@ -26,15 +26,15 @@ export class MessageComponent implements OnInit {
     this.otherUser = false;
     this.isModerator = false;
     this.subscription = new Subscription();
-    this.currentTime$ = new Observable<string>();
+    this.currentTime$ = new Observable<Date>();
   }
 
   ngOnInit() {
     this.currentTime$ = interval(1000).pipe(
       map(() => {
         const currentDate = new Date();
-        currentDate.setTime(currentDate.getTime() - 10000); // Aggiungi 10.000 ms
-        return currentDate.toISOString();
+        currentDate.setTime(currentDate.getTime() - 10000);
+        return currentDate;
       })
     );
     this.subscription = this.currentTime$.subscribe(value => {
@@ -52,6 +52,13 @@ export class MessageComponent implements OnInit {
       this.currentUser = true;
       this.otherUser = false;
     }
+    if (this.message.isInvite == 'PENDING') {
+      while(this.message.isInvite == 'PENDING') {
+        const msgToms = Date.parse(this.message.time);
+        const changeTime  = (Number(this.currentTime$) - msgToms)
+        setTimeout(() => {this.message.isInvite = 'OUTDATED'}, changeTime);
+      }
+    }
   }
 
   ngOnDestroy() {
@@ -59,16 +66,6 @@ export class MessageComponent implements OnInit {
       this.subscription.unsubscribe();
     }
   }
-
-  async OutdatedStatus() {
-    console.log(this.currentTime$, this.message.time);
-    if (this.currentTime$ > this.message.time) {
-      this.message.isInvite = 'OUTDATED';
-      return true;
-    }
-    return false;
-  }
-
 
   redirectToGame() {
   this.router.navigate(['/transcendence/pong'], {queryParams: {invited: this.message.msg}})
