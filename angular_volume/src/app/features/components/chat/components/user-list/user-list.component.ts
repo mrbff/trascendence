5072af5254b0d9d60cb630d46777ab8ff5c3d365
Giuseppe Raiolo <giusepperaiolo@hotmail.com>
2024-02-20 16:02:23 +0100

    fix invite button emit?

--- angular_volume/src/app/features/components/chat/components/user-list/user-list.component.ts
+++ angular_volume/src/app/features/components/chat/components/user-list/user-list.component.ts
@@ -134,10 +134,6 @@ export class UserListComponent implements OnInit, OnDestroy{
 
 		ngOnChanges(changes: SimpleChanges): void {
 			if (JSON.stringify(changes['pending'].currentValue) != JSON.stringify(changes['pending'].previousValue)) {
-			console.log(`-------------Pending changed---------------`);
-			console.log('curr',changes['pending'].currentValue);
-			console.log('prev', changes['pending'].previousValue);
-			console.log(`---------------------------------`);
 			this.pending = changes['pending'].currentValue;
 			this.pendingInvite();
 			}
